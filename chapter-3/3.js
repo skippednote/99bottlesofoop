5abe8cf27896ba328fb805db6e75e23ee53fecdc
Bassam Ismail <skippednote@skippednote.dev>
2021-03-28 15:28:35 +0530

    chapter 3

---
+++ chapter-3/3.js
@@ -0,0 +1,51 @@
+const { downTo } = require("../chapter-1/utils");
+
+class Bottle {
+  verse(number) {
+    switch (number) {
+      case 0:
+        return (
+          `No more bottles of milk on the wall, ` +
+          `no more bottles of milk.\n` +
+          `Go to the store and buy some more, ` +
+          `99 bottles of milk on the wall.\n`
+        );
+      case 1:
+        return (
+          `1 bottle of milk on the wall, ` +
+          `1 bottle of milk.\n` +
+          `Take it down and pass it around, ` +
+          `no more bottles of milk on the wall.\n`
+        );
+      default:
+        return (
+          `${number} bottles of milk on the wall, ` +
+          `${number} bottles of milk.\n` +
+          `Take one down and pass it around, ` +
+          `${number - 1} ${this.container(number - 1)} of milk on the wall.\n`
+        );
+    }
+  }
+
+  verses(upper, lower) {
+    return downTo(upper, lower)
+      .map((i) => this.verse(i))
+      .join("\n");
+  }
+
+  song() {
+    return this.verses(99, 0);
+  }
+
+  container(number) {
+    if (number === 1) {
+      return "bottle";
+    } else {
+      return "bottles";
+    }
+  }
+}
+
+module.exports = {
+  Bottle,
+};
